6c84858b8d75a9e1a564b72a61d8a40b32b2cac8
Christopher Fox <cfox1290@gmail.com>
2018-03-08 20:52:59 -0700

    hergh

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const io = socketIO(app.listen(port,  console.log(`this server is running on por
 
 massive(process.env.CONNECTION_STRING).then(db => app.set('db', db));
 
-app.use("/js", express.static(__dirname + "/build"));
+app.use("/js", express.static(__dirname + "/../build"));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -210,5 +210,6 @@ app.get('/api/getOrbOrder', controller.getOrbOrder);
 
 const path = require('path')
 app.get('*', (req, res)=>{
+    console.log('None Met')
   res.sendFile(path.join(__dirname, '../build/'));
 })
